154288b907ea808b80ceb4e8a713af7a90c49a2e
KhalilKhunji <khalilkhunji@gmail.com>
2024-07-28 09:45:42 +0300

    recipe index

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const User = require('../models/user.js');
 const Recipe = require('../models/recipe.js');
 
-
+router.get('/', async (req, res) => {
+    const recipes = await Recipe.find();
+    res.render('recipes/index.ejs', {recipes});
+});
 
 module.exports = router;
