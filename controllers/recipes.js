c7ef0d285d7e350d3a23faf01d3814f982a00a0e
KhalilKhunji <khalilkhunji@gmail.com>
2024-07-28 11:27:29 +0300

    added create recipe

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -9,4 +9,21 @@ router.get('/', async (req, res) => {
     res.render('recipes/index.ejs', {recipes});
 });
 
+router.get('/new', async (req, res) => {
+    res.render('recipes/new.ejs');
+});
+
+router.post('/create', async (req, res) => {
+    const currentUser = await User.findById(req.session.user._id);
+    try {
+        const newRecipe = await Recipe.create(req.body);
+        newRecipe.owner.push(currentUser._id);
+        await newRecipe.save();
+        res.redirect("/recipes");
+    } catch (err) {
+        console.log(err);
+        res.redirect("/");
+    };
+});
+
 module.exports = router;
